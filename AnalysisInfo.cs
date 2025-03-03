af291e65e8693f8ec5390e96db88cd48de64a742
René Mihula <mihula@provys.com>
2025-03-03 14:33:39 +0100

    last version

--- AnalysisInfo.cs
+++ AnalysisInfo.cs
@@ -1,28 +1,65 @@
 ﻿using System.Collections.Generic;
 using System.Text;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace WebCommentsAnalysis
 {
+    internal static class PrintInfo
+    {
+        public static string Print(AnalysisFileInfo analysisFileInfo)
+        {
+            if (analysisFileInfo == null) return null;
+
+            var result = new StringBuilder();
+            foreach (var c in analysisFileInfo.Classes)
+            {
+                result.Append(Print(c));
+            }
+            return result.ToString();
+        }
+
+        public static string Print(AnalysisClassInfo analysisClassInfo)
+        {
+            if (analysisClassInfo == null) return null;
+
+            var result = new StringBuilder();
+
+            var analysisFileInfo = analysisClassInfo.FileInfo;
+            var fileString = analysisFileInfo != null ? $"{analysisFileInfo.ModuleName}\t{analysisFileInfo.FileName}" : "\t";
+            var classString = $"{analysisClassInfo.EntityName}\t{analysisClassInfo.ClassName}\t{analysisClassInfo.FormClassName}\t{analysisClassInfo.ClassStatus}\t{analysisClassInfo.ClassSubStatus}";
+            var methods = analysisClassInfo.Methods;
+            if (methods.Count == 0) methods = new List<AnalysisMethodInfo>() {
+                new AnalysisMethodInfo { 
+                    MethodName = analysisClassInfo.ClassName,
+                    MethodStatus = analysisClassInfo.ClassStatus,
+                    MethodSubStatus = analysisClassInfo.ClassSubStatus,
+                    MethodLineCount = 1
+                } 
+            };
+
+            foreach (var m in methods)
+            {
+                result.AppendLine($"{fileString}\t{classString}\t{Print(m)}");
+            }
+
+            return result.ToString();
+        }
+        public static string Print(AnalysisMethodInfo analysisMethodInfo)
+        {
+            if (analysisMethodInfo == null) return null;
+            return $"{analysisMethodInfo.MethodName}\t{analysisMethodInfo.MethodStatus}\t{analysisMethodInfo.MethodSubStatus}\t{analysisMethodInfo.MethodLineCount}";
+        }
+    }
+
     internal class AnalysisFileInfo
     {
         public string FullPath { get; set; }
         public string ModuleName { get; set; }
         public string FileName { get; set; }
         public List<AnalysisClassInfo> Classes { get; set; }
-
         public override string ToString()
         {
-            var result = new StringBuilder();
-            var fileString = $"{ModuleName}\t{FileName}";
-            foreach (var c in Classes)
-            {
-                //if (c.EntityName != null) continue; // vypis jen co jsem nenasel
-                foreach (var m in c.Methods)
-                {
-                    result.AppendLine($"{fileString}\t{c.ToString()}\t{m.ToString()}");
-                }
-            }
-            return result.ToString();
+            return PrintInfo.Print(this);
         }
     }
 
@@ -33,11 +70,11 @@ namespace WebCommentsAnalysis
         public string EntityName { get; set; }
         public string ClassStatus { get; set; }
         public string ClassSubStatus { get; set; }
+        public AnalysisFileInfo FileInfo { get; set; }
         public List<AnalysisMethodInfo> Methods { get; set; }
-
         public override string ToString()
         {
-            return $"{EntityName}\t{ClassName}\t{FormClassName}\t{ClassStatus}\t{ClassSubStatus}";
+            return PrintInfo.Print(this);
         }
     }
 
@@ -47,10 +84,9 @@ namespace WebCommentsAnalysis
         public string MethodStatus { get; set; }
         public string MethodSubStatus { get; set; }
         public int MethodLineCount { get; set; }
-
         public override string ToString()
         {
-            return $"{MethodName}\t{MethodStatus}\t{MethodSubStatus}\t{MethodLineCount}";
+            return PrintInfo.Print(this);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using Microsoft.CodeAnalysis;
@@ -26,7 +27,7 @@ namespace WebCommentsAnalysis
             var path = args.Length > 0 ? args[0] : @"C:\Provys\pvysdev\_net\src\Provys\Modules";
 
             // Test Set the module name to "KEC" if provided, otherwise use an empty string
-            var moduleOnly = args.Length > 1 ? args[1] : "KEC";
+            var moduleOnly = args.Length > 1 ? args[1] : "";
             // Set the module name to if provided, otherwise use an empty string
             //var moduleOnly = args.Length > 1 ? args[1] : "";
 
@@ -36,6 +37,9 @@ namespace WebCommentsAnalysis
 
             // Get all the .cs files in the specified path and its subdirectories
             string[] files = Directory.GetFiles(path, "*.cs", SearchOption.AllDirectories);
+
+            // result
+            var fileInfos = new List<AnalysisFileInfo>();
 
             // Process each file
             foreach (string file in files)
@@ -49,11 +53,181 @@ namespace WebCommentsAnalysis
                 // Process the file and get the result
                 var fi = ProcessFile(filePath);
 
-                // Print the result
-                var fiAsString = fi.ToString();
-                if (!string.IsNullOrEmpty(fiAsString))
-                    Console.WriteLine(fi);
+                fileInfos.Add(fi);
+
+                //Console.WriteLine($"{filePath}: {fi.Classes.Count()}");
+
+                //// Print the result
+                //var fiAsString = fi.ToString();
+                //if (!string.IsNullOrEmpty(fiAsString))
+                //    Console.WriteLine(fi);
             }
+
+            // necham si jen ty co nemaji entity ted
+            var classesWithEntity = fileInfos.SelectMany(fi => fi.Classes).Where(c => !string.IsNullOrEmpty(c.EntityName)).ToList();
+            var classesWithoutEntity = fileInfos.SelectMany(fi => fi.Classes).Where(c => string.IsNullOrEmpty(c.EntityName)).ToList();
+
+            // dohledat entity
+            var classesToProcess = new Queue<AnalysisClassInfo>(classesWithoutEntity);
+            var classesUnprocessed = new Queue<AnalysisClassInfo>();
+            var lastSizeOfUnprocessed = classesToProcess.Count();
+
+            while (true)
+            {
+                //Console.WriteLine($"Unprocessed classes: {lastSizeOfUnprocessed}");
+                while (classesToProcess.Count() > 0)
+                {
+                    var c = classesToProcess.Dequeue();
+
+                    // zkus najit podobnou classu
+                    var ci = FindSimilarClass(c.ClassName, classesWithEntity);
+                    if (ci != null)
+                    {
+                        c.EntityName = ci.EntityName;
+                        classesWithEntity.Add(c);
+                        continue;
+                    }
+
+                    // zkus tipnout entitu podle classname
+                    var entityName = GuessEntityByClassName(c.ClassName);
+                    if (!string.IsNullOrEmpty(entityName))
+                    {
+                        c.EntityName = entityName;
+                        classesWithEntity.Add(c);
+                        continue;
+                    }
+
+                    // nenasel jsem
+                    classesUnprocessed.Enqueue(c);
+                }
+
+                if (lastSizeOfUnprocessed == classesUnprocessed.Count())
+                {
+                    break; // neni uz co resit
+                }
+
+                // prehazu a jedu znova
+                lastSizeOfUnprocessed = classesUnprocessed.Count();
+                classesToProcess = classesUnprocessed;
+                classesUnprocessed = new Queue<AnalysisClassInfo>();
+            }
+
+            //Console.WriteLine($"WITH ENTITY: {classesWithEntity.Count()}");
+            //foreach (var c in classesWithEntity)
+            //{
+            //    var ciAsString = c.ToString();
+            //    if (!string.IsNullOrEmpty(ciAsString))
+            //        Console.WriteLine(ciAsString);
+            //}
+
+            //for (var i = 0; i < 10; i++) Console.WriteLine();
+
+            //Console.WriteLine($"WITHOUT ENTITY: {classesUnprocessed.Count()}");
+            //foreach (var c in classesUnprocessed)
+            //{
+            //    var ciAsString = c.ToString();
+            //    if (!string.IsNullOrEmpty(ciAsString))
+            //        Console.Write(ciAsString);
+            //}
+
+            // ALL
+            Console.WriteLine("MODULE\tFILENAME\tENTITY\tCLASSNAME\tFORMCLASSNAME\tCLASSSTATUS\tCLASSSUBSTATUS\tMETHODNAME\tSTATUS\tSUBSTATUS\tSIZE");
+            foreach (var fi in fileInfos)
+            {
+                var fiAsString = PrintInfo.Print(fi);
+                if (!string.IsNullOrEmpty(fiAsString))
+                    Console.Write(fi);
+            }
+        }
+
+        private static AnalysisClassInfo FindSimilarClass(string className, List<AnalysisClassInfo> classesWithEntity)
+        {
+            // pokud je to partial class tak nemusi mit u sebe formclassname a pritom uz ho znam i entitu
+            var partialClass = classesWithEntity.FirstOrDefault(x => x.ClassName == className);
+            if (partialClass != null) return partialClass;
+
+            if (className.EndsWith("EditList"))
+            {
+                var ci = classesWithEntity.FirstOrDefault(x => x.ClassName == className.Replace("EditList", "List"));
+                if (ci != null) return ci;
+            }
+            if (className.EndsWith("Module"))
+            {
+                var ci = classesWithEntity.FirstOrDefault(x => x.ClassName == className.Replace("Module", "List"));
+                if (ci != null) return ci;
+            }
+            if (className.EndsWith("Module"))
+            {
+                var ci = classesWithEntity.FirstOrDefault(x => x.ClassName == className.Replace("Module", "Detail"));
+                if (ci != null) return ci;
+            }
+            if (className.EndsWith("SwitchModule"))
+            {
+                var ci = classesWithEntity.FirstOrDefault(x => x.ClassName == className.Replace("SwitchModule", "List"));
+                if (ci != null) return ci;
+            }
+            if (className.EndsWith("CommonModule"))
+            {
+                var ci = classesWithEntity.FirstOrDefault(x => x.ClassName == className.Replace("CommonModule", "Module"));
+                if (ci != null) return ci;
+            }
+            if (className.EndsWith("BaseModule"))
+            {
+                var ci = classesWithEntity.FirstOrDefault(x => x.ClassName == className.Replace("BaseModule", "Module"));
+                if (ci != null) return ci;
+            }
+            if (className.EndsWith("CrossModule"))
+            {
+                var ci = classesWithEntity.FirstOrDefault(x => x.ClassName == className.Replace("CrossModule", "Module"));
+                if (ci != null) return ci;
+            }
+            if (className.EndsWith("CrossModule"))
+            {
+                var ci = classesWithEntity.FirstOrDefault(x => x.ClassName == className.Replace("CrossModule", "List"));
+                if (ci != null) return ci;
+            }
+            if (className.EndsWith("SwitchModule"))
+            {
+                var ci = classesWithEntity.FirstOrDefault(x => x.ClassName == className.Replace("SwitchModule", "Detail"));
+                if (ci != null) return ci;
+            }
+            if (className.EndsWith("DetailSwitch"))
+            {
+                var ci = classesWithEntity.FirstOrDefault(x => x.ClassName == className.Replace("DetailSwitch", "Detail"));
+                if (ci != null) return ci;
+            }
+            if (className.StartsWith("Common"))
+            {
+                var ci = classesWithEntity.FirstOrDefault(x => x.ClassName == className.Replace("Common", ""));
+                if (ci != null) return ci;
+            }
+            if (className.StartsWith("Custom"))
+            {
+                var ci = classesWithEntity.FirstOrDefault(x => x.ClassName == className.Replace("Custom", ""));
+                if (ci != null) return ci;
+            }
+            if (className.Contains("Base"))
+            {
+                var ci = classesWithEntity.FirstOrDefault(x => x.ClassName == className.Replace("Base", ""));
+                if (ci != null) return ci;
+            }
+
+            return null;
+        }
+
+        private static string GuessEntityByClassName(string className)
+        {
+            string[] entityNamesSubstrings = {
+                "TECHINFO", "RESALLOC", "SUBJSUBTYPE", "REFERENCE", "REPORT", "COMPPFL", "COMPCFG", "XCOMP", "CONFOBJ",
+                "COMMORDER", "COMMBLOCK", "TXEVENT", "TXSLOT", "PRODUSAGE", "VODREQ"
+            };
+            var upClassName = className.ToUpperInvariant();
+            foreach (var entityName in entityNamesSubstrings)
+            {
+                if (upClassName.Contains(entityName))
+                    return entityName;
+            }
+            return null;
         }
 
         /// <summary>
@@ -82,7 +256,8 @@ namespace WebCommentsAnalysis
                 // Create a new AnalysisClassInfo object to store information about the class
                 var classInfo = new AnalysisClassInfo
                 {
-                    ClassName = $"class {classNode.Identifier.ValueText}",
+                    FileInfo = fileInfo,
+                    ClassName = classNode.Identifier.ValueText, // $"class {classNode.Identifier.ValueText}",
                     Methods = new List<AnalysisMethodInfo>()
                 };
 
